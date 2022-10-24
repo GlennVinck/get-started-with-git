0a56c401fd159164c99ba8fcc572a0cd0f5908a1
GlennVinck <vinck_glenn@hotmail.com>
2022-10-24 14:43:01 +0200

    added something else

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 function doSomething() {
     console.log("i'm doing something");
+    doSomethingElse();
 }
 
 doSomething();
+
+function doSomethingElse() {
+    console.log("i'm doing something Else");
+}
